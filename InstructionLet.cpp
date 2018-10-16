ff5146ae78e10e31e8918191462d308acffe9d8d
Eric Safari <saf_39@hotmail.com>
2018-10-16 02:14:51 -0400

    Instruction LET (Version 3)
    Implémentation de la Machine à état(MSA) de la fonction "operation" et meilleure traitement des paranthèses.
    Implémentation de la fonction "trouverAdresse2" et traitement des caractères.

--- InstructionLet.cpp
+++ InstructionLet.cpp
@@ -109,17 +109,50 @@ unsigned short trouverAddresse( char variable )
 	return addrVar;
 }
 
-int f(char * variablePtr)
+unsigned short trouverAddresse2(char * variablePtr)
 {
 	unsigned short addrVar;
 
 	// 1 - Trouver les addresses des 2 variables
 	if( strlen(variablePtr) == 1 )
 	{
-		if( (isalpha(*variablePtr) != 0) && (islower(*variablePtr) != 0) )
+		if( isalpha(*variablePtr) != 0 )
 		{
-			addrVar = trouverAddresse( *variablePtr );
-			//........ À Completer
+			if (islower(*variablePtr) != 0)
+			{
+				// La variable n'a pas encore été utilisée...
+				// IL FAUT L'INITIALISER À ZÉRO!!!!!!!!!!
+				if (Instruction::getMap().count(*variablePtr) == 0)
+				{
+					addrVar = Instruction::getAddress();
+
+					Instruction::setMap(*variablePtr, addrVar);
+					Instruction::setAddress(addrVar + 1);
+				}
+				else // La Variable a été utilisée avant
+				{
+					// Il faut trouver son Adresse dans la Mémoire
+					addrVar = Instruction::getMap()[*variablePtr];
+				}
+			}
+			else
+			{
+				// Variables temporaires
+				switch (*variablePtr)
+				{
+					case 'T':	addrVar = 99; // TEMP1
+						break;
+
+					case 'S':	addrVar = 98; // TEMP2
+						break;
+
+					case 'A':	addrVar = 97; // PARANTHÈSE
+						break;
+					default:	cout << "ERREUR: VARIABLE(" << *variablePtr << ") INVALIDE!!!!!!!!!";
+						_exit(-7);
+						break;
+				}
+			}
 		}
 		else if(isdigit(*variablePtr))
 		{
@@ -130,8 +163,6 @@ int f(char * variablePtr)
 
 			//listeInstruction.insert(listeInstruction.begin(), listeTemp.begin(), listeTemp.end());
 		}
-
-		return *variablePtr;
 	}
 	else
 	{
@@ -145,7 +176,7 @@ int f(char * variablePtr)
 			listeTemp = genererNombre(*variablePtr);
 			addrVar = 51; // CONSTANTE(S)
 
-			return variable;
+			//return variable;
 		}
 		catch( invalid_argument& ArgInval )
 		{
@@ -158,6 +189,8 @@ int f(char * variablePtr)
 			throw;
 		}
 	}
+
+	return addrVar;
 }
 
 list<int> evalOperation(char * variable1Ptr, char operateur, char * variable2Ptr)
@@ -168,8 +201,8 @@ list<int> evalOperation(char * variable1Ptr, char operateur, char * variable2Ptr
 	int instruction;
 
 	// 1 - Trouver les addresses des 2 variables
-	addrVar1 = trouverAddresse( *variable1Ptr );
-	addrVar2 = trouverAddresse( *variable1Ptr);
+	addrVar1 = trouverAddresse2(variable1Ptr);
+	addrVar2 = trouverAddresse2(variable2Ptr);
 	
 	// 2 - LOAD VAR1 & VAR2
 	instruction = 2000 + addrVar1;
@@ -205,9 +238,10 @@ list<int> evalOperation(char * variable1Ptr, char operateur, char * variable2Ptr
 list<int> evalExpression( list<char *> expression )
 {
 	list <int> listeInstruction;
-	char var1,
-		 var2,
-		 oper;
+	char * var1Ptr,
+		 * var2Ptr,
+		 * operPtr;
+	//char oper;
 	unsigned short	addrVar1,
 					addrVar2;
 	int instruction;
@@ -217,34 +251,30 @@ list<int> evalExpression( list<char *> expression )
 	{
 		if (it != expression.begin())
 		{
-			var1 = 'T';
+			*var1Ptr = 'T';
 		}
 		else
 		{
-			var1 = *(*it);
-			//it++;
-
+			var1Ptr = *it;
 		}
 
 		it++;
-		oper = *(*it);
+		operPtr = *it;
 
-		if( !(oper != '+' || oper != '-' || oper != '*' || oper != '/') || (strlen(*it) > 1) )
+		// L'Itérateur pointe sur l'Opérateur qui suit...
+		if( !(*operPtr != '+' || *operPtr != '-' || *operPtr != '*' || *operPtr != '/') || (strlen(operPtr) > 1) )
 		{
-			cout << "ERREUR: L'OPÉRATION(" << oper << ") N'EST PAS VALIDE!!!!!!!!!";
+			cout << "ERREUR: L'OPÉRATION(" << *operPtr << ") N'EST PAS VALIDE!!!!!!!!!";
 			_exit(-9);
 		}
 		
+		// Deuxième variable
 		it++;
-		var2 = *(*it);
+		var2Ptr = *it;
 
 		// Le résultat se trouve dans l'Accumulateur
-		list<int> listeTemp = evalOperation(var1, oper, var2);	
+		list<int> listeTemp = evalOperation( var1Ptr, *operPtr, var2Ptr );
 		listeInstruction.insert( listeInstruction.end(), listeTemp.begin(), listeTemp.end() );
-		
-		//// L'Itérateur pointe sur l'Opérateur
-		//// qui suit...
-		//it++;
 	}
 
 	return listeInstruction;
@@ -264,13 +294,13 @@ list<int> InstructionLet::operation(void)
     char cle,
          cleVarDep;
     vector<char> vecteurInstruction;
-	vector<char *> vecteurInstructionPtr;
+	list<char *> listInstructionPtr;
 	char var1,
 		var2,
 		oper;
     
     enum Etat_t {	
-					INIT, OPERATION, PARANTHESE1, PARANTHESE2, OPER_PRIO, EVAL_OPER, EVAL_EXPR,
+					INIT, OPERATION, PARANTHESE, OPER_PRIO, EVAL_OPER, EVAL_EXPR,
 					ETAT_FINAL, REMPLACER_PARA, TEMP, TEMP1, TEMP2, REMPLACER_TEMP, FIN
 				};
 
@@ -285,10 +315,12 @@ list<int> InstructionLet::operation(void)
     // -------------------------------------------------------
     //    cle = *getParam().c_str();
 	unsigned short taille = getParam().size();
-	equation = new char[taille + 1];
 
+	equation = new char[taille + 1];
+	memset(equation, 0, taille + 1);
 
 	strncpy(equation, getParam().c_str(), taille);
+	//equation[taille - 1] = 0;
     
     // Extraction de la Var. dép.
     cleVarDep = *strtok( equation, " " );
@@ -350,7 +382,7 @@ list<int> InstructionLet::operation(void)
 			}
 
 			//vecteurInstruction.push_back(cle);
-			vecteurInstructionPtr.push_back( clePtr );
+			listInstructionPtr.push_back( clePtr );
 		}
 		else
 		{
@@ -360,26 +392,36 @@ list<int> InstructionLet::operation(void)
 			if ( *clePtr == '(' )
             {
                 nbParantOuvr++;
-				//clePtr++;
+
+				char * paraTemp = new char[2];
+				paraTemp[0] = *clePtr;
+				paraTemp[1] = 0;
+				listInstructionPtr.push_back(paraTemp);
 
 				char * nb = new char[n];
 				strncpy(nb, clePtr + 1, n - 1);
-				nb[n] = 0;
+				nb[n - 1] = 0;
 
-				vecteurInstructionPtr.push_back(nb);
+				//listInstructionPtr.push_back(clePtr); // '('
+				listInstructionPtr.push_back(nb);
             }
 			else
 			{
 				if ( *(clePtr + n - 1) == ')' )
 				{
 					nbParantFerm++;
-					//clePtr--;
+
+					char * paraTemp = new char[2];
+					paraTemp[0] = *(clePtr + n -1);
+					paraTemp[1] = 0;
+					listInstructionPtr.push_back(paraTemp);
 
 					char * nb = new char[n];
 					strncpy(nb, clePtr, n - 1);
-					nb[n] = 0;
+					nb[n - 1] = 0;
 
-				vecteurInstructionPtr.push_back(nb);
+					listInstructionPtr.push_back(nb);
+					//listInstructionPtr.push_back(clePtr + n - 1);
 				}
 				else
 				{
@@ -387,13 +429,9 @@ list<int> InstructionLet::operation(void)
 					strncpy(nb, clePtr, n);
 					nb[n] = 0;
 
-				vecteurInstructionPtr.push_back(nb);
+					listInstructionPtr.push_back(nb);
 				}
-				vecteurInstruction.push_back( *clePtr );
 			}
-            
-            /*vecteurInstruction.push_back( *clePtr );
-            vecteurInstruction.push_back( *( clePtr + 1 ) );*/
 		}
 	}
     
@@ -406,96 +444,256 @@ list<int> InstructionLet::operation(void)
 
 	Etat_t etat = INIT;
 
-	switch (etat)
+	// L'ITÉRATEUR POINTE SUR LA VARIABLE 1
+	list<char *>::iterator listInstructionPtrIter = listInstructionPtr.begin(),
+						   itListInstruction1	  = listInstructionPtr.begin(),
+						   itListInstruction2,
+						   iterParantheseFerm,
+						   iterParantheseOuvr;
+
+	list<int> listeInstrSMLTemp;
+	
+	list<char *> expressionTemp;
+	char * var1Ptr,
+		 * var2Ptr,
+		 * operPtr;
+
+	unsigned short addrVar1 = 100,
+				   addrVar2 = 100;
+
+	int instructionTemp = -1;
+	char Temp1 = 'T',
+		Temp2 = 'S',
+		Paranthese = 'A';
+
+	while( etat != FIN )
 	{
-	case INIT:
-		etat = OPERATION;
+		switch( etat )
+		{
+			case INIT:
+				// L'ITÉRATEUR POINTE SUR LE 1ER OPÉRATEUR
+				listInstructionPtrIter++;
 
-	case OPERATION:
-		if (expression(var2))
-		{
-			listeInstr = evalExpression(var1, oper, var2);
-			etat = TEMP1;
-		}
-		else
-		{
-			// LOAD
-		}
+				//oper = *(*listInstructionPtrIter);
+				etat = OPERATION;
+				break;
 
-	case PARANTHESE1:
-		if (paraFerm)
-		{
-			etat = PARANTHESE2;
-		}
-		else
-		{
-			etat = OPER_PRIO;
-		}
+			case OPERATION:
+				oper = *(*listInstructionPtrIter);
 
-	case PARANTHESE2:
-		if (paraOuvr)
-		{
-			etat = PARANTHESE2;
-		}
-		else
-		{
-			//etat = OPER_PRIO;
-			//EXIT...
-			cout << "Paranthèse ouvrante manquante!!!!???????" << endl;
-			_exit(-5);
-		}
+				if( (oper != '(') && (oper != ')') )
+				{
+					// Isoler Une Expression
+					while (expression(oper))
+					{
+						advance(listInstructionPtrIter, 2);
+						oper = *(*listInstructionPtrIter);
+					}
+					// À la Sortie du WHILE, l'Itérateur pointe sur un '+' ou un '-'
 
-	case OPER_PRIO:
-		if (multDiv)
-		{
-			etat = EVAL_EXPR;
-		}
-		else
-		{
-			etat = EVAL_OPER;
-		}
+					itListInstruction2 = listInstructionPtrIter;
+					expressionTemp.insert(expressionTemp.begin(), itListInstruction1, itListInstruction2);
 
-	case EVAL_EXPR:
-		if (finiExpr)
-		{
-			etat = TEMP;
-		}
+					// Le 1er Opérateur est un '*' ou un '/'
+					if (expressionTemp.size() > 1)
+					{
+						listeInstrSMLTemp = evalExpression(expressionTemp);
+						expressionTemp.clear();
 
-	case EVAL_OPER:
-		if (finiOper)
-		{
-			etat = ETAT_FINAL;
-		}
+						etat = TEMP;
+						break;
+					}
+					else
+					{
+						addrVar1 = trouverAddresse2(*expressionTemp.begin());
+						instructionTemp = 2000 + addrVar1; // LOAD VAR1
 
-	case ETAT_FINAL:
-		if (paraFerm)
-		{
-			etat = REMPLACER_PARA;
-		}
-		else
-		{
-			etat = FIN;
-		}
+						listeInstrSMLTemp.push_back(instructionTemp);
+					}
 
-	case REMPLACER_PARA:
-		etat = PARANTHESE1;
+					// L'Itérateur pointe sur l'Opérateur qui suit
+					// le '+' ou le '-', peut-être une PARANTHÈSE...
+					advance(listInstructionPtrIter, 2);
+					oper = *(*listInstructionPtrIter);
 
-	case TEMP:
-		if (!temp1Util)
-		{
-			etat = TEMP1;
+					// L'Opérateur est un '+' ou un '-'
+					if (!expression(oper))
+					{
+						addrVar2 = trouverAddresse2(*expressionTemp.begin());
+
+						if (oper == '+')
+						{
+							instructionTemp = 3000 + addrVar2; // ADD VAR2
+						}
+						else
+						{
+							instructionTemp = 3100 + addrVar2; // SUB VAR2
+						}
+						listeInstrSMLTemp.push_back(instructionTemp);
+						/*var1Ptr = *listInstructionPtr.begin();
+
+						advance(listInstructionPtrIter, -2);
+						operPtr = *listInstructionPtrIter;
+						listInstructionPtrIter++;
+						var2Ptr = *listInstructionPtrIter;
+						listInstructionPtrIter++;
+
+						listeInstrSMLTemp = evalOperation(var1Ptr, *operPtr, var2Ptr);*/
+					}
+					else // encore un '*' ou un '/'
+					{
+						//// Sauvegarder VAR1
+						//listeInstrSMLTemp = evalExpression(expressionTemp);
+						//expressionTemp.clear();
+
+						etat = TEMP;
+					}
+
+					//listInstructionPtrIter++;
+					//itListInstruction1 = listInstructionPtrIter; // itListInstruction1 = itListInstruction2 + 1
+				}
+				else
+				{
+					if( oper != ')' )
+					{
+						paraFerm = true;
+						iterParantheseFerm = listInstructionPtrIter;
+						listInstructionPtrIter--;
+						etat = PARANTHESE;
+					}
+					//else // (oper != '(')
+					//{
+					//	//
+					//}
+				}
+
+				break;
+
+			case PARANTHESE:
+				while( (listInstructionPtrIter != listInstructionPtr.begin())  && !paraOuvr )
+				{
+					if( *(*listInstructionPtrIter) != '(' )
+					{
+						listInstructionPtrIter--;
+					}
+					else
+					{
+						iterParantheseOuvr = listInstructionPtrIter;
+						paraOuvr = true;
+
+						//etat = OPER_PRIO;
+						break;
+					}
+				}
+
+				// PEUT_ÊTRE QUE...
+				// listInstructionPtrIter = listInstructionPtr.begin()
+				if( *(*listInstructionPtrIter)  && !paraOuvr )
+				{
+					//EXIT...
+					cout << "Paranthèse ouvrante manquante!!!!???????" << endl;
+					_exit(-5);
+				}
+				else
+				{
+					etat = OPER_PRIO;
+					paraOuvr = false;
+				}
+
+				break;
+
+			case OPER_PRIO:
+				if (multDiv)
+				{
+					expressionTemp.insert( expressionTemp.begin(), ++iterParantheseOuvr, iterParantheseFerm );
+					etat = EVAL_EXPR;
+				}
+				else
+				{
+					etat = EVAL_OPER;
+				}
+
+				break;
+
+			case EVAL_EXPR:
+				//while( !finiExpr )
+				//{
+				//
+				//	//etat = TEMP;
+				//}
+				evalExpression(expressionTemp);
+
+				etat = TEMP;
+				break;
+
+			case EVAL_OPER:
+				if( !finiOper )
+				{
+					//evalLesOperations();
+					etat = ETAT_FINAL;
+				}
+
+				break;
+
+			case ETAT_FINAL:
+				if (paraFerm)
+				{
+					etat = REMPLACER_PARA;
+				}
+				else
+				{
+					etat = FIN;
+				}
+
+				break;
+
+			case REMPLACER_PARA:
+				listInstructionPtr.erase(iterParantheseOuvr, ++iterParantheseFerm);
+				listInstructionPtr.push_front( &Paranthese );
+
+				//etat = PARANTHESE1;
+				etat = PARANTHESE;
+
+				break;
+
+			case TEMP:
+				if( !temp1Util )
+				{
+					instructionTemp = 2100 + 99; // STORE LE RESULTAT DANS TEMP1
+					listeInstrSMLTemp.push_back(instructionTemp);
+					temp1Util = true;
+
+					etat = TEMP1;
+				}
+				else
+				{
+					instructionTemp = 2100 + 98; // STORE LE RESULTAT DANS TEMP2
+					listeInstrSMLTemp.push_back(instructionTemp);
+
+					etat = TEMP2;
+				}
+
+				break;
+
+			case TEMP1:
+				etat = OPERATION;
+				break;
+
+			case TEMP2:
+				etat = REMPLACER_TEMP;
+				break;
+
+			case REMPLACER_TEMP:
+				listInstructionPtr.erase(itListInstruction1, itListInstruction2);
+				listInstructionPtr.push_front( &Temp1 );
+
+				etat = TEMP1;
+				break;
+
+			default:
+				cout << "ÉTAT INCONNU!!!!!!!!!!!!!!!!" << endl;
+				_exit(-6);
 		}
-		else
-		{
-			etat = TEMP2;
-		}
-		
-	case TEMP1:
-	case TEMP2:
-	case REMPLACER_TEMP:
-	default:
-		cout << "ÉTAT INCONNU!!!!!!!!!!!!!!!!" << endl;
-		_exit(-6);
 	}
     
     return listeInstr;
